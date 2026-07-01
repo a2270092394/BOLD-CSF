be6b56aa658e2381f5a18df20b8f8abe0812f8b6
LXD227 <a2270092394@gmail.com>
2026-07-02 03:32:00 +0800

    step2.m

--- step2.m
+++ step2.m
@@ -1,23 +1,23 @@
-% 询问使用者预处理数据的文件夹位置
-source_path = uigetdir('', '请选择预处理数据文件夹');
+% Prompt user to select the source folder containing preprocessed data
+source_path = uigetdir('', 'Please select the preprocessed data folder');
 
-% 询问使用者需要复制到的文件夹位置
-dest_path = uigetdir('', '请选择目标文件夹');
+% Prompt user to select the destination folder
+dest_path = uigetdir('', 'Please select the destination folder');
 
-% 询问使用者list.txt的位置
-[list_file, list_path] = uigetfile('*.txt', '请选择list.txt文件');
+% Prompt user to select the list.txt file
+[list_file, list_path] = uigetfile('*.txt', 'Please select the list.txt file');
 list = textread(fullfile(list_path, list_file), '%s');
 
-% 询问使用者global功能磁共振图像文件夹位置
-funImg_global_path = uigetdir(source_path, '请选择global功能磁共振图像文件夹');
+% Prompt user to select the global fMRI image folder
+funImg_global_path = uigetdir(source_path, 'Please select the global fMRI image folder');
 
-% 询问使用者CSF功能磁共振图像文件夹位置
-funImg_CSF_path = uigetdir(source_path, '请选择CSF功能磁共振图像文件夹');
+% Prompt user to select the CSF fMRI image folder
+funImg_CSF_path = uigetdir(source_path, 'Please select the CSF fMRI image folder');
 
-% 询问使用者结构磁共振图像文件夹位置
-t1Img_path = uigetdir(source_path, '请选择结构磁共振图像 (T1Img) 文件夹');
+% Prompt user to select the structural MRI image folder
+t1Img_path = uigetdir(source_path, 'Please select the structural MRI (T1Img) folder');
 
-% 遍历文件夹列表
+% Loop through the folder list
 for i = 1:length(list)
     folder_name = list{i};
 	
@@ -25,57 +25,56 @@ for i = 1:length(list)
     CSF_source_folder_FunImg = fullfile(funImg_CSF_path, folder_name);	
     source_folder_T1ImgCoreg = fullfile(t1Img_path, folder_name);
 	
-    % 处理 globalFunImg 文件夹中的文件
+    % Process files in the globalFunImg folder
     cd(global_source_folder_FunImg);
     
-    % 获取文件夹中的唯一 .nii 文件
+    % Get the unique .nii file in the folder
     files = dir('*.nii');
     old_name = files(1).name;
     new_name = 'gfmri.nii';
     
-    % 生成目标文件夹路径
+    % Generate destination folder path
     dest_folder = fullfile(dest_path, folder_name);
     
-    % 如果目标文件夹不存在，则创建
+    % Create destination folder if it does not exist
     if ~exist(dest_folder, 'dir')
         mkdir(dest_folder);
     end
     
-    % 复制文件到目标文件夹
+    % Copy file to destination folder
     copyfile(fullfile(global_source_folder_FunImg, old_name), fullfile(dest_folder, old_name));
     
-    % 切换到目标文件夹并重命名文件
+    % Switch to destination folder and rename the file
     cd(dest_folder);
     movefile(old_name, new_name);
 	
-	% 处理CSF_FunImg文件夹中的文件
+	% Process files in the CSF_FunImg folder
     cd(CSF_source_folder_FunImg);
     
-    % 获取文件夹中的唯一 .nii 文件
+    % Get the unique .nii file in the folder
     files = dir('*.nii');
     old_name = files(1).name;
     new_name = 'cfmri.nii';
     
-    % 复制文件到目标文件夹
+    % Copy file to destination folder
     copyfile(fullfile(CSF_source_folder_FunImg, old_name), fullfile(dest_folder, old_name));
     
-    % 切换到目标文件夹并重命名文件
+    % Switch to destination folder and rename the file
     cd(dest_folder);
     movefile(old_name, new_name);
     
-    % 处理 T1ImgCoreg 文件夹中的文件
+    % Process files in the T1ImgCoreg folder
     cd(source_folder_T1ImgCoreg);
     
-    % 获取文件夹中的唯一 .nii 文件
+    % Get the unique .nii file in the folder
     files = dir('*.nii');
     old_name = files(1).name;
     new_name = 'T1.nii';
     
-    % 复制文件到目标文件夹
+    % Copy file to destination folder
     copyfile(fullfile(source_folder_T1ImgCoreg, old_name), fullfile(dest_folder, old_name));
     
-    % 切换到目标文件夹并重命名文件
+    % Switch to destination folder and rename the file
     cd(dest_folder);
     movefile(old_name, new_name);
 end
-
